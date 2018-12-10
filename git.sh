be4f1aad47d6b0b2f0bd2e838973166a26f03f38
hongwei8sun <hongwei8sun@qq.com>
2018-12-10 17:48:22 -0500

    hello

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m "message:12/7/2018"
+    git commit -m "hello"
     git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
