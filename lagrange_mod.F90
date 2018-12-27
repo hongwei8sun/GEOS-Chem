74b42364e0e6a15b6089b5b35721bbfe4e1e937a
hongwei8sun <hongwei8sun@qq.com>
2018-12-27 09:39:16 -0500

    ~/GC_lagrange/Code.12.0.0/GeosCore

--- lagrange_mod.F90
+++ lagrange_mod.F90
@@ -1,11 +1,9 @@
 !------------------------------------------------------------------------------
 !                  GEOS-Chem Global Chemical Transport Model                  !
-!------------------------------------------------------------------------------
-!
+!-----------------------------------------------------------------------------!
+
 MODULE Lagrange_Mod
-!
-! !USES:
-!
+
   USE precision_mod
   USE CMN_SIZE_mod
   USE PhysConstants   ! Physical constants: Re, PI, PI_180
@@ -29,12 +27,13 @@ MODULE Lagrange_Mod
 !
 CONTAINS
 
+
 !\\
 ! !INTERFACE:
 !
   SUBROUTINE init_lagrange( am_I_root )
 
-    LOGICAL,        INTENT(IN)    :: am_I_Root   ! Are we on the root CPU?
+    LOGICAL,        INTENT(IN)    :: am_I_Root   ! Are we on the root CPU
 !    REAL(f4), POINTER :: Ptr2D(:,:)
 
     allocate(box_lon(n_boxes_max))
@@ -46,29 +45,34 @@ CONTAINS
 
     box_lon    = 0.0e+0_fp
     box_lat    = 0.0e+0_fp
+    box_lev    = 0.0e+0_fp
     box_width  = 0.0e+0_fp
     box_depth  = 0.0e+0_fp
     box_length = 0.0e+0_fp
 
   END SUBROUTINE init_lagrange
 
-!#
-  SUBROUTINE run_lagrange(am_I_Root, state_met, state_chm, input_opt)
 
-    USE state_chm_mod, ONLY : chmstate
-    USE state_met_mod, ONLY : metstate
-    USE input_opt_mod, ONLY : OptInput
+!#
+  SUBROUTINE run_lagrange(am_I_Root, State_Met, State_Chm, Input_Opt)
+
+    USE Input_Opt_Mod, ONLY : OptInput
+    USE PhysConstants, ONLY : PI, Re 
+
+    USE State_Chm_Mod, ONLY : ChmState
+    USE State_Met_Mod, ONLY : MetState
+
+    USE GC_GRID_MOD,   ONLY : XEDGE, YEDGE, XMID, YMID                  ! choose XEDGE or XMID ??        
+    USE CMN_SIZE_Mod,  ONLY : IIPAR, JJPAR, LLPAR, DLAT, DLON
+    ! DLAT( IIPAR, JJPAR, LLPAR ), DLON( IIPAR, JJPAR, LLPAR )
+    ! XEDGE  ( IM+1, JM,   L ), YEDGE  ( IM,   JM+1, L ), IM=IIPAR, JM=JJPAR
 
     logical, intent(in) :: am_I_Root
-    TYPE(MetState), intent(in) :: state_met
-    TYPE(ChmState), intent(inout) :: state_chm
-    TYPE(OptInput), intent(in) :: input_opt
+    TYPE(MetState), intent(in) :: State_Met
+    TYPE(ChmState), intent(inout) :: State_Chm
+    TYPE(OptInput), intent(in) :: Input_Opt
 
-    REAL :: Dt           = 600.0e+0_fp    ! TS_CONV is the time step we need here ? 
-
-    REAL(fp) :: REarth   = 6.375e+6_fp
-    REAL(fp) :: Pi       = 3.14159265358979323e+0_fp     ! PI:Double-Precision value of PI          
-!    REAL(fp) :: Pi_180   = Pi / 180e+0_fp                ! PI_180 : Number ofradians per degree
+    REAL :: Dt           = 600.0e+0_fp                   ! TS_CONV is the time step we need here ? 
 
     integer :: i_box
 
@@ -89,17 +93,20 @@ CONTAINS
     real(fp), pointer :: omeg(:,:,:)
     real(fp), pointer :: p_lev(:,:,:)
 
+      ! LONEDG(I) = GET_XEDGE( IIPAR+1, 1, 1 )
+
+
     ! Establish pointers
     u => State_Met%U   ! figure out state_met%U is based on lat/lon or modelgrid(i,j)
     v => State_Met%V
     omeg => State_Met%OMEGA
     p_lev => State_Met%PMID  !Pressure (w/r/t moist air) at level centers (hPa)
-    ! Run Lagrangian advection HERE
 
+    ! Run Lagrangian advection HERE
     do i_box = 1,n_boxes_max
        curr_lon = box_lon(i_box)
        curr_lat = box_lat(i_box)
-       curr_pressure = 20.0e+0_fp ! hPa
+       curr_pressure = box_lev(i_box)         ! 20.0e+0_fp ! hPa
        !curr_pressure = box_pressure(i_box)
 
        if (curr_lon > 180.0) cycle   ! Input lon should be between (-180,180)
@@ -111,8 +118,8 @@ CONTAINS
        i_lat = find_latitude(curr_lat) !  
        i_lev = find_plev(curr_pressure,i_lon,i_lat,p_lev)
 
-       dbox_lon = (Dt*u(i_lon,i_lat,i_lev)) /(2.0*Pi*REarth*cos(curr_lat*Pi/180.0))*360.0
-       dbox_lat = (Dt*v(i_lon,i_lat,i_lev)) / (Pi*REarth)*180.0
+       dbox_lon = (Dt*u(i_lon,i_lat,i_lev)) /(2.0*PI*Re*cos(curr_lat*PI/180.0))*360.0
+       dbox_lat = (Dt*v(i_lon,i_lat,i_lev)) / (PI*Re)*180.0
        dbox_lev = Dt*omeg(i_lon,i_lat,i_lev)
 
        box_lon(i_box) = box_lon(i_box) + dbox_lon
@@ -130,22 +137,25 @@ CONTAINS
 
   END SUBROUTINE run_lagrange
 
-  integer function find_longitude(curr_lon)
+
+  integer function find_longitude(curr_lon, DLON, IIPAR, XMID)
     implicit none
-    real :: curr_lon
-    find_longitude = INT(( ANINT(curr_lon/5.0)*5.0 - (-180.0) )/5.0 +1)
+    real :: curr_lon, DLON(:,:,:)  !注意DLON,XMID的维度
+    find_longitude = INT( ANINT( (curr_lon-(XMID(1)))/DLON ) )+1
     ! for 5*4 horizontal resolution run (-180:5:175); ANINT 四舍五入函数
     return
   end function
 
-  integer function find_latitude(curr_lat)
+
+  integer function find_latitude(curr_lat, DLAT, JJPAR, YMID)
     implicit none
-    real :: curr_lat
-    find_latitude = INT(( ANINT((curr_lat+2.0)/4.0)*4.0-2.0 - (-90) )/4.0 +1)
+    real :: curr_lat, DLAT
+    find_latitude = INT( ANINT( (curr_lat-(YMID(1)))/DLAT ) )+1
     ! since we won't spread aerosols in polar region, ignore 89 and -89 in polar
     ! region. (-89,-86:4:86,89))
     return
   end function
+
 
   integer function find_plev(curr_pressure,i_lon,i_lat,p_lev)
     implicit none
