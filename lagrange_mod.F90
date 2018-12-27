2ac8c9bfc8127692fcf13ece1a331abe24211277
hongwei8sun <hongwei8sun@qq.com>
2018-12-27 15:21:39 -0500

    ~/GC_lagrange/Code.12.0.0/GeosCore

--- lagrange_mod.F90
+++ lagrange_mod.F90
@@ -9,9 +9,9 @@ MODULE Lagrange_Mod
   USE PhysConstants   ! Physical constants: Re, PI, PI_180
 
   IMPLICIT NONE
-!
-! !PUBLIC MEMBER FUNCTIONS:
-!
+
+
+  ! !PUBLIC MEMBER FUNCTIONS:
   PUBLIC :: init_lagrange
   PUBLIC :: run_lagrange
   PUBLIC :: cleanup_lagrange
@@ -24,17 +24,14 @@ MODULE Lagrange_Mod
   real(fp), allocatable :: box_depth(:)
   real(fp), allocatable :: box_width(:)
   real(fp), allocatable :: box_length(:)
-!
+
 CONTAINS
 
 
-!\\
-! !INTERFACE:
-!
+
   SUBROUTINE init_lagrange( am_I_root )
 
     LOGICAL,        INTENT(IN)    :: am_I_Root   ! Are we on the root CPU
-!    REAL(f4), POINTER :: Ptr2D(:,:)
 
     allocate(box_lon(n_boxes_max))
     allocate(box_lat(n_boxes_max))
@@ -53,7 +50,7 @@ CONTAINS
   END SUBROUTINE init_lagrange
 
 
-!#
+
   SUBROUTINE run_lagrange(am_I_Root, State_Met, State_Chm, Input_Opt)
 
     USE Input_Opt_Mod, ONLY : OptInput
@@ -72,7 +69,7 @@ CONTAINS
     TYPE(ChmState), intent(inout) :: State_Chm
     TYPE(OptInput), intent(in) :: Input_Opt
 
-    REAL :: Dt           = 600.0e+0_fp                   ! TS_CONV is the time step we need here ? 
+    REAL :: Dt           = 600.0e+0_fp          ! TS_CONV is the time step we need here ? 
 
     integer :: i_box
 
@@ -83,6 +80,8 @@ CONTAINS
     real :: curr_lon
     real :: curr_lat
     real :: curr_pressure
+    real :: X_mid2
+    real :: Y_mid2
 
     real :: dbox_lon
     real :: dbox_lat
@@ -93,8 +92,10 @@ CONTAINS
     real(fp), pointer :: omeg(:,:,:)
     real(fp), pointer :: p_lev(:,:,:)
 
-      ! LONEDG(I) = GET_XEDGE( IIPAR+1, 1, 1 )
-
+    real :: Dx
+    real :: Dy
+    real(fp), pointer :: X_mid(:)    
+    real(fp), pointer :: Y_mid(:)       
 
     ! Establish pointers
     u => State_Met%U   ! figure out state_met%U is based on lat/lon or modelgrid(i,j)
@@ -102,25 +103,43 @@ CONTAINS
     omeg => State_Met%OMEGA
     p_lev => State_Met%PMID  !Pressure (w/r/t moist air) at level centers (hPa)
 
+    Dx = DLON(1,1,1)
+    Dy = DLAT(1,1,1)
+    X_mid => XMID(:,1,1)
+    Y_mid => YMID(1,:,1)   ! Use second YMID, because sometimes YMID(2)-YMID(1) is not DLAT
+
     ! Run Lagrangian advection HERE
     do i_box = 1,n_boxes_max
-       curr_lon = box_lon(i_box)
-       curr_lat = box_lat(i_box)
-       curr_pressure = box_lev(i_box)         ! 20.0e+0_fp ! hPa
-       !curr_pressure = box_pressure(i_box)
 
-       if (curr_lon > 180.0) cycle   ! Input lon should be between (-180,180)
+       ! make sure the location is not out of range
+       if (curr_lon > 180.0) cycle  
        if (curr_lon < -180.0) cycle
-       if (curr_lat > 89.0) cycle    ! Input lat should be between (-89,89)
-       if (curr_lat < -89.0) cycle
+       if (curr_lat > Y_mid(JJPAR)) cycle 
+       if (curr_lat < Y_mid(1)) cycle
 
-       i_lon = find_longitude(curr_lon) !  WRITE THIS FUNCTION
-       i_lat = find_latitude(curr_lat) !  
+       do while (box_lon(i_box) > X_mid(IIPAR))
+          box_lon(i_box) = box_lon(i_box) - 360.0
+       end do
+
+       do while (box_lon(i_box) < X_mid(1))
+          box_lon(i_box) = box_lon(i_box) + 360.0
+       end do
+
+
+       curr_lon      = box_lon(i_box)
+       curr_lat      = box_lat(i_box)
+       curr_pressure = box_lev(i_box)        ! hPa
+       X_mid2         = X_mid(2)
+       Y_mid2         = Y_mid(2)
+
+       i_lon = find_longitude(curr_lon, Dx, X_mid2)
+       i_lat = find_latitude(curr_lat, Dy, Y_mid2) 
        i_lev = find_plev(curr_pressure,i_lon,i_lat,p_lev)
 
-       dbox_lon = (Dt*u(i_lon,i_lat,i_lev)) /(2.0*PI*Re*cos(curr_lat*PI/180.0))*360.0
-       dbox_lat = (Dt*v(i_lon,i_lat,i_lev)) / (PI*Re)*180.0
-       dbox_lev = Dt*omeg(i_lon,i_lat,i_lev)
+
+       dbox_lon = (Dt*u(i_lon,i_lat,i_lev)) / (2.0*PI*Re*cos(curr_lat*PI/180.0)) * 360.0
+       dbox_lat = (Dt*v(i_lon,i_lat,i_lev)) / (PI*Re) * 180.0
+       dbox_lev = Dt * omeg(i_lon,i_lat,i_lev)
 
        box_lon(i_box) = box_lon(i_box) + dbox_lon
        box_lat(i_box) = box_lat(i_box) + dbox_lat
@@ -134,27 +153,41 @@ CONTAINS
     nullify(u)
     nullify(v)
     nullify(omeg)
+    nullify(p_lev)
 
   END SUBROUTINE run_lagrange
 
 
-  integer function find_longitude(curr_lon, DLON, IIPAR, XMID)
+  integer function find_longitude(curr_lon,  Dx,  X_mid2)
     implicit none
-    real :: curr_lon, DLON(:,:,:)  !注意DLON,XMID的维度
-    find_longitude = INT( ANINT( (curr_lon-(XMID(1)))/DLON ) )+1
-    ! for 5*4 horizontal resolution run (-180:5:175); ANINT 四舍五入函数
+    real :: curr_lon, Dx, X_mid2
+    find_longitude = INT(ANINT( (curr_lon - (X_mid2 - Dx)) / Dx ))+1
+    ! for lon: Xmid_Sec - Dx = Xmid_first
     return
   end function
 
 
-  integer function find_latitude(curr_lat, DLAT, JJPAR, YMID)
+  integer function find_latitude(curr_lat, Dy, Y_mid2)
     implicit none
-    real :: curr_lat, DLAT
-    find_latitude = INT( ANINT( (curr_lat-(YMID(1)))/DLAT ) )+1
-    ! since we won't spread aerosols in polar region, ignore 89 and -89 in polar
-    ! region. (-89,-86:4:86,89))
+    real :: curr_lat, Dy, Y_mid2
+    find_latitude = INT(ANINT( (curr_lat - (Y_mid2 - Dy)) / Dy ))+1
+    ! for lat: (Ymid_Sec - Dy) may be different from Ymid_first
+    ! region for 4*5: (-89,-86:4:86,89))
     return
   end function
+
+
+  ! which find_latitude function is better ?? 
+
+  !  integer function find_latitude(curr_lat, Dy, YMID)
+  !    implicit none
+  !    real :: YMID
+  !    real :: curr_lat, Dy
+  !    integer :: locate(1)
+  !    locate = MINLOC(abs( YMID(1,:,1) - curr_lat ))
+  !    find_plev = locate(1)
+  !    return
+  !  end function
 
 
   integer function find_plev(curr_pressure,i_lon,i_lat,p_lev)
@@ -164,14 +197,13 @@ CONTAINS
     integer :: i_lon, i_lat
     integer :: locate(1)
     locate = MINLOC(abs( p_lev(i_lon,i_lat,:)-curr_pressure ))
-    ! Notice that minimum difference in pressure (hPa) might not the minimum
-    ! difference in distance (km)
-    find_plev = INT(locate(1))
+    ! Notice that minimum difference in pressure (hPa) might not 
+    ! the minimum difference in distance (km)
+    find_plev = locate(1)
     return
   end function
 
 
-!#
 
   subroutine cleanup_lagrange()
     if (allocated(box_lon)) deallocate(box_lon)
@@ -182,5 +214,5 @@ CONTAINS
     if (allocated(box_width)) deallocate(box_width)
   end subroutine cleanup_lagrange
 
-!EOC
+
 END MODULE Lagrange_Mod
