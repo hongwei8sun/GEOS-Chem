90332e3baaf1762d4ecaa3d595f04597e89821de
hongwei8sun <hongwei8sun@qq.com>
2019-01-07 14:57:57 -0500

    write out into a txt for lagrange

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@
     git add .
     git status
     git commit -m "hello"
-    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
+#    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
