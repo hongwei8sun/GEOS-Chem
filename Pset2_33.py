87caa88c47692cf0cc5da7ec3b01b94b479e8ae7
hongwei8sun <hongwei8sun@qq.com>
2019-02-11 22:31:13 -0500

    .

--- Pset2_33.py
+++ Pset2_33.py
@@ -19,34 +19,43 @@ ps  = f_txt[:,3]
 f = interp1d(sol, ps)
 
 # the new data's time intervel is every 
-sol_new = np.linspace(10, 1258, 12481)
-#sol_new = np.linspace(sol[0], sol[len(sol)-1], num=len(sol), endpoint=True)
+#sol_new = np.linspace(10, 1258, 12481)
+
+sol_new = np.linspace(10, 1258, 124801)
 ps_new  = f(sol_new)
-print(sol_new)
+
+xx = np.arange(len(sol_new))
 
 # Fourier transform
-ps_fft = abs(np.fft.fft(ps_new))
-xf = np.arange(len(ps_new))        # Frequency
-
+ps_fft = np.fft.fft(ps_new)
+f = abs(ps_fft)/len(ps_new)
+print('ps_fft',f[1247:1249])
 #print(sol)
 
-plt.figure(figsize=(9,5.5))
+plt.figure(figsize=(9,11))
 
-#plt.figure()
-plt.subplot(211)
-#plt.plot(sol1, ps1, 'C3', lw=3)
-plt.scatter(sol_new, ps_new, s=3)
-plt.title('Surface pressure vs. Time')
-plt.xlabel('Sol (entire sampling period)')
-plt.ylabel('PS1')
 
 # Scatter plot on top of lines
-plt.subplot(212)
+plt.subplot(311)
 #plt.plot(sol2, ps2, 'C3', zorder=1, lw=3)
-plt.plot(xf[3:], ps_fft[3:])
+plt.plot(xx[1:], f[1:])
+plt.yscale('log')
+plt.title('Fourier transform')
 #plt.tight_layout()
 #plt.xlabel('Sol (a few martain sols)')
 #plt.ylabel('PS1')
+
+plt.subplot(312)
+#plt.plot(sol2, ps2, 'C3', zorder=1, lw=3)
+plt.plot(xx[1:2000], f[1:2000])
+plt.scatter(xx[1248], f[1248],facecolor='red',zorder=2)
+plt.title('Diurnal Frequencies (red point)')
+
+plt.subplot(313)
+#plt.plot(sol2, ps2, 'C3', zorder=1, lw=3)
+plt.plot(xx[1:30], f[1:30])
+plt.scatter(xx[4], f[4],facecolor='red',zorder=2)
+plt.title('Annual Frequencies (red point)')
 
 plt.savefig('set2_33.png')
 
