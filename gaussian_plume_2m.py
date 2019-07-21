687214fdeb423a2ca5ba26a2f015e3468fa0d7da
hongwei8sun <hongwei8sun@qq.com>
2019-07-21 10:53:57 -0400

    .

---
+++ gaussian_plume_2m.py
@@ -0,0 +1,57 @@
+from mpl_toolkits.basemap import Basemap, cm
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.ticker
+from matplotlib.mlab import bivariate_normal
+import math
+
+
+#
+PI = 3.14
+ 
+Sigma_h0 = 10.0  # [m]
+Sigma_v0 = 10.0
+
+D_h = 1.0  # [m2/s]
+D_v = 1.0
+
+C0 = 100.0* PI*10.0*10.0 # [kg/m3 * m2 = kg/m]
+
+x = [1.0]
+z = [1.0]
+C = [0.0]
+
+N_time = 5000
+N_ring = 500
+
+D_r = 2.0
+
+for i in range(N_ring-1):
+	x.append( (i+1)*D_r + 1.0 )
+	z.append( (i+1)*D_r + 1.0 )
+	C.append( 0.0 )
+
+
+Sigma_h = Sigma_h0
+Sigma_v = Sigma_v0
+
+for i in range(N_ring):
+	C[i] = C0 / (2.0*PI*Sigma_h*Sigma_v) * math.exp(-0.5*( x[i]**2/Sigma_h**2 + z[i]**2/Sigma_v**2 ))
+
+print(C)
+
+
+for t in range(1,N_time,1):
+	
+	#print(t)
+	Sigma_h = math.sqrt(Sigma_h0**2 + 2.0 * D_h * t)
+	Sigma_v = math.sqrt(Sigma_v0**2 + 2.0 * D_v * t)
+	
+	for i in range(N_ring):
+		C[i] = C0 / (2.0*PI*Sigma_h*Sigma_v) * math.exp(-0.5*( x[i]**2/Sigma_h**2 + z[i]**2/Sigma_v**2 ))
+	print(C)
+
+
+
