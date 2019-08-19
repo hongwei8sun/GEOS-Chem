ce83e6d9a86d22f5d584af406af668afe7dd974b
hongwei8sun <hongwei8sun@qq.com>
2019-08-19 10:14:43 -0400

    lagrangain module results

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m "success! pay attention to CFL condition"
+    git commit -m "lagrangain module results"
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
