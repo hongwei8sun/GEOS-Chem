acd49f17e75e438051786b8dfbdb8356cd3ae321
hongwei8sun <hongwei8sun@qq.com>
2019-08-20 11:53:40 -0400

    new directory png_gif to store png and gif

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " the plume_mod code for distortion  "
+    git commit -m " new directory png_gif to store png and gif  "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
