c01a4ce23ed7bcf60bf482ac0326b3db4edbee90
hongwei8sun <hongwei8sun@qq.com>
2019-08-20 10:07:00 -0400

    distortion of plume cross-section because of wind shear

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " . "
+    git commit -m " distortion of plume cross-section because of wind shear "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
