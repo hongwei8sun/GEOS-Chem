03f32c46ef405a4ed3eb2b42f12620009aa11528
hongwei8sun <hongwei8sun@qq.com>
2019-08-20 10:47:24 -0400

    just delete all the gif before this comment

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " distortion of plume cross-section because of wind shear "
+    git commit -m " just delete all the gif before this comment "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
