122345cdce4c7665dd83992f075652fd2d72af9e
hongwei8sun <hongwei8sun@qq.com>
2019-08-20 11:51:49 -0400

    the plume_mod code for distortion

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " just delete all the gif before this comment "
+    git commit -m " the plume_mod code for distortion  "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
