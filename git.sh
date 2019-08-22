7fbd6d1ff893e87eab5901dcdb4cfb5c4b8fdfb4
hongwei8sun <hongwei8sun@qq.com>
2019-08-22 12:08:50 -0400

    rewrite plume model (polar coordinat) in Python

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " add legend to plume gif "
+    git commit -m " rewrite plume model (polar coordinat) in Python "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
