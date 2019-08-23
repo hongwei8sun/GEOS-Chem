e1c6340cb0bdbdb71dcc4d12fc9a035799729ae5
hongwei8sun <hongwei8sun@qq.com>
2019-08-23 11:16:14 -0400

    .

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " set z=0.0 in gaussian analytical results !!! "
+    git commit -m " . "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
