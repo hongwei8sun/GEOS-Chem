40f02ac571b4e7853b0276feb5526f21710b5c09
hongwei8sun <hongwei8sun@qq.com>
2019-09-06 10:55:12 -0400

    Dt=600s

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " Dt=50s "
+    git commit -m " Dt=600s "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
