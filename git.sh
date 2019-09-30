f90ee6dfa7bf86d569ee6b455783c3e2ea5681cf
hongwei8sun <hongwei8sun@qq.com>
2019-09-30 11:12:48 -0400

    Conventional GEOS-Chem: aircraft releases aerosols one by one

--- git.sh
+++ git.sh
@@ -1,7 +1,8 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " aircraft releases aerosols one by one "
+    git commit -m " Conventional GEOS-Chem: aircraft releases aerosols one by one "
+#    git commit -m " aircraft releases aerosols one by one "
 #    git commit -m " Lagrangian figure: aircraft releases aerosols "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
