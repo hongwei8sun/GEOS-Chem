c459cc83773b892ac8415774b3e93b3912795c74
hongwei8sun <hongwei8sun@qq.com>
2019-11-10 17:39:35 -0500

    plot figure for [mol/cm3]; lon-Z; lat-Z

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
     git add .
     git status
 #    git commit -m " Binary file for Lagrange output "
-    git commit -m "AGU: aircraft release aerosol boxes between (-30N,30N) for 1 month"
+    git commit -m "plot figure for [mol/cm3]; lon-Z; lat-Z"
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
--- seperate2_geos_lagrange_grid_NoInit.py
+++ seperate2_geos_lagrange_grid_NoInit.py
@@ -8,32 +8,78 @@ from matplotlib.mlab import bivariate_normal
 import math
 #pandas
 #------------------------------------------------
-#------------------------------------------------
-FILEDIR = '/n/home12/hongwei/GC_Python/Postdeal_lagrange_points/LAGR_GOES4x5_1mon/'
-
-#NcFile = Dataset(FILEDIR+'Lagrange_Geos_Concentration_12deg.nc','r',format='NETCDF4_CLASSIC')
-NcFile   = Dataset(FILEDIR+'GEOSChem.SpeciesConc_inst.20150101_0000z.nc4','r',format='NETCDF4_CLASSIC')
+FILEDIR2 = '/n/home12/hongwei/GC_Python/Postdeal_lagrange_points/LAGR_GOES4x5_1yr/'
+#NcFile = Dataset(FILEDIR2+'Lagrange_Geos_Concentration_12deg.nc','r',format='NETCDF4_CLASSIC')
+NcFile   = Dataset(FILEDIR2+'GEOSChem.SpeciesConc_inst.20150101_0000z.nc4','r',format='NETCDF4_CLASSIC')
 
 lat             = NcFile.variables['lat']
-print(lat)
 geos            = NcFile.variables['SpeciesConc_PASV1']
-geos_Zsum       = np.sum(geos[:,:,:,:], axis=1)
-geos_Zsum_Xmean = np.sum(geos_Zsum[:,:,:], axis=2)
+lagrange        = NcFile.variables['LAGRG']
 
-lagrange            = NcFile.variables['LAGRG']
-lagrange_Zsum       = np.sum(lagrange[:,:,:,:], axis=1)
-lagrange_Zsum_Xmean = np.sum(lagrange_Zsum[:,:,:], axis=2)
+#------------------------------------------------
+FILEDIR = '/n/home12/hongwei/hongwei/merra2_4x5_standard/'
+
+#------------------------------------------------
+# total air mass in each grid  ------------------
+#------------------------------------------------
+
+AD_file = open(FILEDIR+'State_Met_AD.txt','r')
+
+GC_AD = geos[0,:,:,:]*0.0
+
+Nx = len(geos[0,0,0,:])
+Ny = len(geos[0,0,:,0])
+Nz = len(geos[0,:,0,0])
+
+for ix in range(Nx):
+    for iy in range(Ny):
+        for iz in range(Nz):
+            line = AD_file.readline()
+            GC_AD[iz,iy,ix] = float(line)
+
+
+AREA_file = open(FILEDIR+'State_Met_AREA_M2.txt','r')
+
+GC_AREA = np.zeros( (Nz, Ny, Nx), dtype=float )
+
+for ix in range(Nx):
+    for iy in range(Ny):
+        for iz in range(Nz):
+            line = AREA_file.readline()
+            GC_AREA[iz,iy,ix] = float(line)
+
+###
+geos2           = geos[:,:,:,:]*0.0
+for i in range(len(geos[:,0,0,0])):
+    geos2[i,:,:,:] = geos[i,:,:,:]*GC_AD[:,:,:]
+
+geos_Zmean       = np.mean(geos2[:,:,:,:], axis=1)
+for i in range(len(geos[:,0,0,0])):
+    geos_Zmean[i,:,:] = geos_Zmean[i,:,:]/GC_AREA[0,:,:]/1e4		# [mol/cm2]
+
+geos_Zmean_Xmean = np.mean(geos_Zmean[:,:,:], axis=2)
+
+lagrange2           = lagrange[:,:,:,:]*0.0
+for i in range(len(lagrange[:,0,0,0])):
+    lagrange2[i,:,:,:] = lagrange[i,:,:,:]*GC_AD[:,:,:]
+
+lagrange_Zmean       = np.mean(lagrange2[:,:,:,:], axis=1)
+for i in range(len(lagrange[:,0,0,0])):
+    lagrange_Zmean[i,:,:]   = lagrange_Zmean[i,:,:]/GC_AREA[0,:,:]/1e4*28.97/98.0
+
+
+lagrange_Zmean_Xmean = np.mean(lagrange_Zmean[:,:,:], axis=2)
 
 # plot  -----------------------------------------
 #------------------------------------------------
 
 # time step for ploting is 24 hours (once every day)
-Nt = len(geos_Zsum[:,0,0])
+Nt = len(geos_Zmean[:,0,0])
 print(Nt)
 
-i=0
+i=359
 while i<Nt:
-	plt.figure(figsize=(10,8))
+	plt.figure(figsize=(14,8))
 	
 	plt.subplot(2, 2, 3)
 	#ax = fig.add_axes([0.1,0.1,0.4,0.4])
@@ -51,31 +97,23 @@ while i<Nt:
 	#m.drawcountries()
 	
 	# for geos ==================================================================
-	ny = geos_Zsum.shape[1]; nx = geos_Zsum.shape[2]
+	ny = geos_Zmean.shape[1]; nx = geos_Zmean.shape[2]
 	lons, lats = m.makegrid(nx, ny) # get lat/lons of ny by nx evenly space grid.
 	x, y = m(lons, lats) # compute map proj coordinates.
-	# for GEOS **********
 	print(i)
-	#clevs = [0.1E-10,0.1E-06,0.5E-06,0.1E-05,0.5E-05,0.1E-04]
-	#cmap=plt.cm.get_cmap('Blues', 7)
 	
-	# uneven bounds changes the colormapping:
-#	bounds = np.array([1.0E-12,1.0E-11,1.0E-10,1.0E-09,1.0E-08,1.0E-07])
-#	bounds = np.array([5.0E-11,10.0E-11,15.0E-11,20.0E-11,25.0E-11,30.0E-11])
-	bounds = np.linspace(1.0E-12,1.0E-06,10)
+	bounds = np.linspace(0.0E-14,30.0E-14,11)
 	norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
-	#pcm = ax[1].pcolormesh(X, Y, Z, norm=norm, cmap='RdBu_r')
-	#cs = m.contourf(x, y, pasv_Zsum[i,43,:,:], norm=norm, cmap='Blues')
-	#pasv_ave = pasv1.sum(axis=1)
-	cs = m.pcolormesh(x, y, geos_Zsum[i,:,:], norm=norm, cmap='Reds')
+	cs = m.pcolormesh(x, y, geos_Zmean[i,:,:], norm=norm, cmap='Reds')
 	cs.cmap.set_under('w')
 	cs.set_clim(1.0E-20)
+
 	# add colorbar.
 	fmt = matplotlib.ticker.ScalarFormatter(useMathText=True)
 	fmt.set_powerlimits((0, 0))
 	
 	cbar = m.colorbar(cs,location='bottom',pad="9%",format=fmt)
-	cbar.set_label('mol/mol')
+	cbar.set_label('mol/cm2')
 	
 	plt.title('GEOS-Chem (Concentration)', fontsize=10)
 	#plt.title('GEOS-Chem (Blue/Shaded) & Lagrange (Red/Scatter)')
@@ -84,13 +122,16 @@ while i<Nt:
 	
 	# for GOES distribution ===================================================
 	plt.subplot(2,2,4)
-	plt.plot(geos_Zsum_Xmean[i,:],lat)
+	plt.plot(geos_Zmean_Xmean[i,:],lat)
 #	plt.xscale('log')
-
+	plt.xlim(5.0E-14,40.0E-14)
+	plt.ylim(-90,90)
+	plt.xlabel('Tracer Concentration (mol/cm2)')
+	plt.ylabel('Latitude (deg)')
 
 	# for Lagrange ============================================================
 	plt.subplot(2, 2, 1)
-	
+
 	m = Basemap(projection='cyl',llcrnrlat=-90,urcrnrlat=90,llcrnrlon=-180,urcrnrlon=180)
 	m.drawcoastlines()
 	m.drawparallels(np.arange(-90.,91.,30.))
@@ -102,12 +143,9 @@ while i<Nt:
 	meridians = np.arange(-180.,180.,60.)
 	m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=8)
 	
-#	bounds = np.array([5.0E-06,1.0E-05,5.0E-05,1.0E-04,5.0E-04,1.0E-03,5.0E-03,1.0E-02,5.0E-02,1.0E-01,5.0E-01])
-#	bounds = np.array([1.0E-12,5.0E-12,1.0E-11,5.0E-11,1.0E-10,5.0E-10,1.0E-09,5.0E-09,1.0E-08,5.0E-08,1.0E-07,5.0E-07,1.0E-06])
-#	bounds = np.array([1.0E-12,1.0E-11,1.0E-10,1.0E-09,1.0E-08,1.0E-07])
-	bounds = np.linspace(1.0E-12,1.0E-06,10)
+	bounds = bounds
 	norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
-	cs = m.pcolormesh(x, y, lagrange_Zsum[i,:,:], norm=norm, cmap='Reds')
+	cs = m.pcolormesh(x, y, lagrange_Zmean[i,:,:], norm=norm, cmap='Reds')
 	cs.cmap.set_under('w')
 	cs.set_clim(1.0E-20)
 	# add colorbar.
@@ -115,14 +153,20 @@ while i<Nt:
 	fmt.set_powerlimits((0, 0))
 	
 	cbar = m.colorbar(cs,location='bottom',pad="9%",format=fmt)
-	cbar.set_label('mol/mol')
+	cbar.set_label('mol/cm2')
 	
 	plt.title('Lagrange (air parcels percent)', fontsize=10)
 		
+
 	# for Lagrange Distribution: ==============================================
 	plt.subplot(2,2,2)
-	plt.plot(lagrange_Zsum_Xmean[i,:],lat)
+	plt.plot(lagrange_Zmean_Xmean[i,:],lat)
+
 #	plt.xscale('log')
+	plt.xlim(5.0E-14,40.0E-14)
+	plt.ylim(-90,90)
+	plt.ylabel('Latitude (deg)')
+
 
 	plt.savefig(str(i+1)+'_xy2.png')
 	plt.clf()
