50d1a86e0e789b3519c8216328e888aa251c2dfd
hongwei8sun <hongwei8sun@qq.com>
2020-03-08 22:50:50 -0400

    Correct: put lagrange_write in front of lagrange_run

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " add: minus the extra amount of injected aerosol when dissolve the plume "
+    git commit -m " Correct: put lagrange_write in front of lagrange_run "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
