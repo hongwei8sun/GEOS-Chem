75bd089540c0de434aa5b9eac67fdf5ecbaa3005
hongwei8sun <hongwei8sun@qq.com>
2020-08-24 21:56:55 -0400

    gif for 2D to 1D slab model

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " code for 2D to 1D slab model "
+    git commit -m " gif for 2D to 1D slab model "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
