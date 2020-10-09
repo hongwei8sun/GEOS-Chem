20e64e4b440ed2cec436dc23e242bcc19cd9b196
hongwei8sun <hongwei8sun@qq.com>
2020-10-09 10:36:20 -0400

    plot gif for plume 1D 1mon

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " let all the left plumes dissolved into background grid at the end running "
+    git commit -m " plot gif for plume 1D 1mon "
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
