14a189ef05c9c6d592b7255521c0afbbdab961c6
hongwei8sun <hongwei8sun@qq.com>
2021-04-05 21:08:05 -0400

    phthon

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
     git add .
     git status
-    git commit -m " sensitive test for 2D grid resolution "
+    git commit -m "phthon"
 #    git remote add origin https://github.com/hongwei8sun/GEOS-Chem.git
     git pull origin master
     git push origin master
